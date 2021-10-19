9363aa45c07b9bc29df3486dad08e3d7e964825d
Lu Yuchao <tyutluyc@qq.com>
2021-10-19 03:04:44 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Quick Start with Demo
+### 0. Apply Token
+You need PadLocal token to run this demo. [How to Apply Token](https://github.com/padlocal/wechaty-puppet-padlocal/wiki/How-to-Apply-Token)
+
+### 1. Prepare Node Environment
+[Install Node](https://nodejs.org/), 12/14 LTS version is recommended.
+```
+$ node --version // >= v12.0.0
+``` 
+### 2. Clone the [wechaty-puppet-padlocal-demo](https://github.com/padlocal/wechaty-puppet-padlocal-demo) project.
+
+```
+$ git clone git@github.com:padlocal/wechaty-puppet-padlocal-demo.git
+```
+Then install Node dependencies.
+```
+$ cd wechaty-puppet-padlocal-demo
+$ npm install
+``` 
+
+### 3. Set you PadLocal Token in [`main.ts`](https://github.com/padlocal/wechaty-puppet-padlocal-demo/blob/master/main.ts)
+```ts
+const puppet = new PuppetPadlocal({
+    token: "YOUR_PADLOCAL_TOKEN"
+})
+```
+Then run it:
+```
+$ npm run demo
+```
+![carbon](https://user-images.githubusercontent.com/64943823/117439626-a6cde080-af65-11eb-85a5-815aa422b5c5.png)
---
+++ main.ts
@@ -0,0 +1,56 @@
+import { PuppetPadlocal } from "wechaty-puppet-padlocal";
+import { Contact, log, Message, ScanStatus, Wechaty } from "wechaty";
+
+// 去掉注释，可以完全打开调试日志
+// log.level("silly");
+
+const puppet = new PuppetPadlocal({
+    token: "YOUR_PADLOCAL_TOKEN"
+})
+
+const bot = new Wechaty({
+    name: "TestBot",
+    puppet,
+})
+
+    .on("scan", (qrcode: string, status: ScanStatus) => {
+        if (status === ScanStatus.Waiting && qrcode) {
+            const qrcodeImageUrl = [
+                'https://wechaty.js.org/qrcode/',
+                encodeURIComponent(qrcode),
+            ].join('')
+
+            log.info("TestBot", `onScan: ${ScanStatus[status]}(${status}) - ${qrcodeImageUrl}`);
+
+            require('qrcode-terminal').generate(qrcode, { small: true })  // show qrcode on console
+        } else {
+            log.info("TestBot", `onScan: ${ScanStatus[status]}(${status})`);
+        }
+    })
+
+    .on("login", (user: Contact) => {
+        log.info("TestBot", `${user} login`);
+    })
+
+    .on("logout", (user: Contact, reason: string) => {
+        log.info("TestBot", `${user} logout, reason: ${reason}`);
+    })
+
+    .on("message", async (message: Message) => {
+        log.info("TestBot", `on message: ${message.toString()}`);
+
+        // ding-dong bot
+        if (message.to()?.self() && message.text().indexOf("ding") !== -1) {
+            await message.talker().say(message.text().replace("ding", "dong"));
+        }
+    })
+
+    .on("error", (error) => {
+        log.error("TestBot", 'on error: ', error.stack);
+    })
+
+
+bot.start().then(() => {
+    log.info("TestBot", "started.");
+});
+
